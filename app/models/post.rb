515a363fd423d13445abc4d958fec45598d9723f
Chris Cook <cdani3l.cook@gmail.com>
2018-01-09 14:14:19 -0700

    another one

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
-class Post < ApplicationRecord
+class Post < ActiveRecord::Base
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
-
 end
+
