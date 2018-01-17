b401e22c63c437ea60c1ff8ad601eb0ddaed517e
Chris Cook <cdani3l.cook@gmail.com>
2018-01-17 14:13:06 -0700

    added numbers and users to the user.rb and seeds file

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -16,6 +16,7 @@ class UserDashboard < Administrate::BaseDashboard
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
     type: Field::String.with_options(searchable: false),
+    phone: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
@@ -29,6 +30,7 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
+    :phone,
     :sign_in_count,
     :current_sign_in_at,
     :last_sign_in_at,
@@ -39,6 +41,7 @@ class UserDashboard < Administrate::BaseDashboard
     :created_at,
     :updated_at,
     :type,
+    
   ].freeze
 
   FORM_ATTRIBUTES = [
@@ -46,5 +49,6 @@ class UserDashboard < Administrate::BaseDashboard
     :password,
     :first_name,
     :last_name,
+    :phone
   ].freeze
 end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ Bundler.require(*Rails.groups)
 
 module Overtime
   class Application < Rails::Application
-    
     config.autoload_paths << Rails.root.join("lib")
   end
 end
